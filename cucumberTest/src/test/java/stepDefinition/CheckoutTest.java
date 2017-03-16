7447bbd1f5435c393c8db32c8ca6ec5c2f349029
Thangakumar <dthangakumar@gmail.com>
2017-03-16 16:23:03 -0700

    Log4j implemented

--- cucumberTest/src/test/java/stepDefinition/CheckoutTest.java
+++ cucumberTest/src/test/java/stepDefinition/CheckoutTest.java
@@ -1,37 +1,48 @@
 package stepDefinition;
+import org.apache.log4j.Logger;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class CheckoutTest {
+	
+	private static org.apache.log4j.Logger log = Logger.getLogger(CheckoutTest.class);
+
 
 	@Given("^the user browses the site amazon\\.com$")
 	public void the_user_browses_the_site_amazon_com() throws Throwable {
 
 		System.out.println("the user browses the site amazon");
+		log.info("the user browses the site amazon");
+
 	}
 
 	@When("^the user searches for (.*)$")
 	public void search(String keyword) throws Throwable {
 
 		System.out.println("the user searches for " + keyword);
+		log.info("the user searches for " + keyword);
 	}
 
 	@Then("^the current page will be the search result page$")
 	public void the_current_page_will_be_the_search_result_page() throws Throwable {
 
 		System.out.println("the current page will be the search result page");
+		log.info("the current page will be the search result page");
 	}
 
 	@When("^the user adds the first product in the search result to the cart$")
 	public void the_user_adds_the_first_product_in_the_search_result_to_the_cart() throws Throwable {
 
 		System.out.println("the user adds the first product in the search result to the cart");
+		log.info("the user adds the first product in the search result to the cart");
 	}
 
 	@Then("^the shopping cart has the added product$")
 	public void the_shopping_cart_has_the_added_product() throws Throwable {
 
 		System.out.println("the shopping cart has the added product");
+		log.info("the shopping cart has the added product");
 	}
 }
--- cucumberTest/src/test/java/stepDefinition/HomePageTest.java
+++ cucumberTest/src/test/java/stepDefinition/HomePageTest.java
@@ -1,16 +1,24 @@
 package stepDefinition;
+import org.apache.log4j.Logger;
+
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class HomePageTest {
 
+	private static org.apache.log4j.Logger log = Logger.getLogger(CheckoutTest.class);
+
 	@When("^the user cliks on signin$")
 	public void the_user_cliks_on_signin() throws Throwable {
 		System.out.println("the user cliks on signin");
+		log.info("the user cliks on signin");
+
 	}
 
 	@Then("^the current page will be the signin page$")
 	public void the_current_page_will_be_the_signin_page() throws Throwable {
 		System.out.println("the current page will be the signin page");
+		log.info("the current page will be the signin page");
+
 	}
 }
